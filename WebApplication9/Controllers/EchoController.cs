25391ebe62c2015ba054fbd27e181479d1f3d61a
Даня Ширлин <sirlindaniil@gmail.com>
2025-03-03 11:31:27 +0300

    Добавьте файлы проекта.

---
+++ WebApplication9/Controllers/EchoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace YourNamespace.Controllers
+{
+    [Route("Echo")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public async Task Get()
+        {
+            Response.ContentType = "text/plain";
+            await Response.WriteAsync("GET request received");
+        }
+
+        [HttpPost("Post")]
+        public async Task Post()
+        {
+            Response.ContentType = "text/plain";
+            await Response.WriteAsync("POST request received");
+        }
+
+        [HttpGet("Headers")]
+        public async Task Headers()
+        {
+            Response.ContentType = "application/json";
+            var headers = Request.Headers;
+            await Response.WriteAsJsonAsync(headers);
+        }
+
+        [HttpGet("Query")]
+        public async Task Query()
+        {
+            Response.ContentType = "application/json";
+            var queryParams = Request.Query;
+            await Response.WriteAsJsonAsync(queryParams);
+        }
+
+        [HttpPost("Body")]
+        public async Task Body()
+        {
+            Response.ContentType = "text/plain";
+            using var reader = new StreamReader(Request.Body);
+            var body = await reader.ReadToEndAsync();
+            await Response.WriteAsync(body);
+        }
+    }
+}
